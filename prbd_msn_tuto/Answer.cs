daba91222e96b77f6b04cbbb44db161ded6a30b4
corentin heinix <corentin.heinix@gmail.com>
2021-03-21 09:05:48 +0100

    feat : Ajout de certains modèles

---
+++ prbd_msn_tuto/Answer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PRBD_Framework;
+
+namespace prbd_msn_tuto {
+    public class Answer : EntityBase<Model> {
+        public int AnswerId { get; set; }
+
+        [Required]
+        public virtual Student Student { get; set; }
+
+        [Required]
+        public virtual QuestionQuizz Question { get; set; }
+
+        public virtual ICollection<Proposition> ChoosedProposition { get; set; } = new HashSet<Proposition>();
+    }
+}
