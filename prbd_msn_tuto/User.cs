65cda790663715c06aa807c7fd62b71eb4377e30
Katia Mijares <c.mijareskatia@outlook.com>
2021-03-23 17:38:46 +0100

    refactor(UC_User) : modification d'une simple class en abstract class

--- prbd_msn_tuto/User.cs
+++ prbd_msn_tuto/User.cs
@@ -5,6 +5,29 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace prbd_msn_tuto {
-    class Class1 {
+    class User {
+
+        public User( string name, string firstName, string mail, string password ) {
+        }
+
+        private string name; 
+        public string Name {
+            get; set; 
+        }
+
+        private string firstname;
+        public string FirstName {
+            get; set;
+        }
+
+        private string mail;
+        public string Mail {
+            get; set; 
+        }
+
+        private string password;
+        public string Password {
+            get; set;
+        }
     }
 }
