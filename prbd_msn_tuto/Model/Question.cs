c1e91fbcd41809d2323a906c9ae0e8ecfcb2676c
Soh momo marceline <marcelinealvinemomosoh@yahoo.fr>
2021-03-24 12:15:28 +0100

    modif Question

--- prbd_msn_tuto/Model/Question.cs
+++ prbd_msn_tuto/Model/Question.cs
@@ -5,6 +5,26 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Msn.Model {
-    class Question {
+
+    public enum TypeQuestion {
+        OneAnswer,
+        ManyAnswer
+    }
+
+    public class Question {
+        public int QuestionId { get; set; }
+        public string Enonce { get; set; }
+        public Boolean IsUpdate { get; set; }
+        public Boolean IsDelete { get; set; }
+        public TypeQuestion typeQuestion { get; set; } = TypeQuestion.OneAnswer;
+        public virtual Category category { get; set; }
+
+        public Question(string enonce, Boolean isUpdate, Boolean isDelete) {
+            Enonce = enonce;
+            IsUpdate = isUpdate;
+            IsDelete = isDelete;
+
+
+        }
     }
 }
