82ec439aadaf4588ecc81f6e0a5707240507765e
Soh momo marceline <marcelinealvinemomosoh@yahoo.fr>
2021-03-24 10:42:03 +0100

    reconstruction

--- prbd_msn_tuto/AppMessages.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace prbd_msn_tuto {
-    public enum AppMessages {
-        MSG_NEW_MEMBER,
-        MSG_PSEUDO_CHANGED,
-        MSG_MEMBER_CHANGED,
-        MSG_DISPLAY_MEMBER,
-        MSG_CLOSE_TAB
-    }
-}
