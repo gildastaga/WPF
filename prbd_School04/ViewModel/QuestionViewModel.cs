e21a78b22d1c0005d38a54ad6d7297fae10778b5
Soh momo marceline <marcelinealvinemomosoh@yahoo.fr>
2021-04-20 23:20:05 +0200

    mise à jour

--- prbd_School04/ViewModel/QuestionViewModel.cs
+++ prbd_School04/ViewModel/QuestionViewModel.cs
@@ -13,6 +13,8 @@ namespace School04.ViewModel {
             throw new NotImplementedException();
         }
 
+        public Question Question { get; set; }
+
         private ObservableCollection<CheckProposition> propositions;  // toute liste qu'on doit affiché ds la vue doit etre observable et donc on doit pouvoir remplir cette liste avec la methode LoadTagsChecked()
         public ObservableCollection<CheckProposition> Propositions {
             get => propositions;
@@ -22,22 +24,22 @@ namespace School04.ViewModel {
             }
         }
 
-        public QuestionViewModel() : base() {
-
-
+        public QuestionViewModel(Question question) : base() {
+            Question = question;
+            LoadPropositionsChecked();
         }
 
-        /*private void LoadPropositionsChecked() {
+        private void LoadPropositionsChecked() {
             Propositions = new ObservableCollection<CheckProposition>();   // Pour le moment ma liste est vide,
             foreach (var proposition in App.Context.Propositions)               //on parcours la DbSet de la BD et pour chaque TagName, je creer une new instance sur lui puis je me dde si ma liste de tag contient dejà ce tag; ma méthode contains renvoit true ou false
             {
                 var p = new CheckProposition()                    // on creer une new instance de TagChecked (cette new classe me permet de savoir si un tag a été coché ou pas d'où cette new classe avec un TagName et un boolean pour savoir si oui ou non le tag a été coché ou pas
                 {
                     Name = proposition.Body,                     // je prends le TagName sur lequel je suis
-                    Checked = Question.Context.Propositions.Contains(proposition)
+                    Checked = Question.Propositions.Contains(proposition)
                 };
                 Propositions.Add(p);                              // Enfin j'ajoute ce tag ds la liste.
             }
-        }*/
+        }
     }
 }
