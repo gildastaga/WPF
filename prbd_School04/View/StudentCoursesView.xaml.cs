0ede98ecf84de66dbbc93337edf038b274bb5303
Katia Mijares <c.mijareskatia@outlook.com>
2021-04-27 22:19:24 +0200

    feat : StudentCoursesView construction

---
+++ prbd_School04/View/StudentCoursesView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using PRBD_Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace School04.View {
+    /// <summary>
+    /// Logique d'interaction pour StudentCoursesView.xaml
+    /// </summary>
+    public partial class StudentCoursesView : WindowBase {
+        public StudentCoursesView() {
+            InitializeComponent();
+        }
+    }
+}
