2b3935adb1fe17a2e4fae2f96fbbaafec588aeae
Katia Mijares <c.mijareskatia@outlook.com>
2021-05-04 18:41:43 +0200

    probleme de taille

--- prbd_School04/View/EditCourseView.xaml.cs
+++ prbd_School04/View/EditCourseView.xaml.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PRBD_Framework;
 
 namespace School04.View {
-    class EditCourseView {
+    public partial class EditCourseView : UserControlBase {
     }
 }
