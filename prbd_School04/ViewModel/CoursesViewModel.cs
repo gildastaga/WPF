c3d56119e1f286f83f4a5a4e01e5511c661d3f0f
Katia Mijares <c.mijareskatia@outlook.com>
2021-05-04 23:25:40 +0200

    refactor : ajout du filtre pour les courses

--- prbd_School04/ViewModel/CoursesViewModel.cs
+++ prbd_School04/ViewModel/CoursesViewModel.cs
@@ -12,14 +12,24 @@ namespace School04.ViewModel {
     class CoursesViewModel : ViewModelCommon {
 
         private ObservableCollection<Course> courses;
+
         public ObservableCollection<Course> Courses {
             get => courses;
-            set => SetProperty<ObservableCollection<Course>>(ref courses, value);
+            //set => SetProperty<ObservableCollection<Course>>(ref courses, value);
+            set {
+                courses = value;
+                RaisePropertyChanged(nameof(courses)); //mis à jour de l'affichage
+            }
+        }
+        public string Filter {
+            get; set;
+        }
+        public ICommand ApplyFilter {
+            get; set;
         }
         public ICommand DisplayCourseDetails {
             get; set;
         }
-
         public void makeList() {
             //Affiche la bonne liste des cours : soit le user connecté est un prof 
             // et donc affiche les cours que ce prof donne
@@ -34,14 +44,45 @@ namespace School04.ViewModel {
         }
         public CoursesViewModel() : base() {
             makeList();
+            //possède une valeur qui est une référence vers une nouvelle instance de la classe ApplyFilterCommand
+            ApplyFilter = new ApplyFilterCommand(ApplyFilterAction);
 
             DisplayCourseDetails = new RelayCommand<Course>(course => {
-                Console.WriteLine(course.Title); 
+                Console.WriteLine(course.Title);
                 NotifyColleagues(AppMessages.MSG_DISPLAY_COURSE, course);
             });
         }
+        private void ApplyFilterAction() {
+            Console.WriteLine("Search clicked! " + Filter);
+            var query = from m in Context.Courses
+                        where m.Title.Contains(Filter)
+                        select m;
+            Courses = new ObservableCollection<Course>(query);
+            Console.WriteLine($"{query.Count()} courses found");
+        }
+
         protected override void OnRefreshData() {
             // Pour plus tard
         }
+    }
+    public class ApplyFilterCommand : ICommand {
+        private Action action;
+        public event EventHandler CanExecuteChanged;
+        //On veut que cette commande interroge le modèle pour retourner une liste filtrée de cours
+        // et que l'affichage soit ensuite mis-à-jour pour afficher cette liste filtrée
+        public ApplyFilterCommand( Action action ) {
+            this.action = action;
+        }
+        //retourne un booléen qui est utilisé par le composant visuel (ici le bouton) 
+        //pour savoir s'il doit être actif et permettre l'exécution de la commande, ou pas
+        //si on retourne false, on verra que notre bouton sera désactivé et ne déclenchera
+        //aucune action si on clique dessus
+        public bool CanExecute( object parameter ) {
+            return true;
+        }
+        public void Execute( object parameter ) {
+            action();
+        }
+    
     }
 }
