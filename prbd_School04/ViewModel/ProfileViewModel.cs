295a9bda9feedff584f228ad655c9afe93f4a2a9
Katia Mijares <c.mijareskatia@outlook.com>
2021-05-14 22:12:19 +0200

    refactor : binding dans la ProfileView

--- prbd_School04/ViewModel/ProfileViewModel.cs
+++ prbd_School04/ViewModel/ProfileViewModel.cs
@@ -7,7 +7,46 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace School04.ViewModel {
-    class ProfileViewModel : ViewModelBase<ModelSchool04> {
+    class ProfileViewModel : ViewModelCommon {
+        private User person;
+
+        public User Person {
+            get => CurrentUser;
+        }
+
+        public ProfileViewModel() {
+            person = CurrentUser;
+            Name = Person.Name;
+            FirstName = Person.FirstName;
+            Profile = Person.Profile; 
+        }
+        public string Name {
+            get {
+                return Person?.Name;
+            }
+            set {
+                Person.Name = value;
+                RaisePropertyChanged(nameof(Name));
+            }
+        }
+        public string FirstName {
+            get {
+                return Person?.FirstName;
+            }
+            set {
+                Person.FirstName = value;
+                RaisePropertyChanged(nameof(FirstName));
+            }
+        }
+        public string Profile {
+            get {
+                return Person?.Profile;
+            }
+            set {
+                Person.Profile = value;
+                RaisePropertyChanged(nameof(Profile));
+            }
+        }
         protected override void OnRefreshData() {
             // Pour plus tard
         }
