501bab079bfebd53c5de084e4bc93bb012d50370
Soh momo marceline <marcelinealvinemomosoh@yahoo.fr>
2021-05-27 20:36:56 +0200

    mise à jour de AppMessages.cs

--- prbd_School04/AppMessages.cs
+++ prbd_School04/AppMessages.cs
@@ -2,27 +2,30 @@
     public enum AppMessages {
         MSG_NEW_COURSE,
         MSG_NEW_QUIZZ,
+        MSG_NEW_QUESTION,
         //MSG_NEW_CATEGORY,
         NEW_STUDENT_ADDED,
         MSG_DISPLAY_COURSE,
         MSG_DISPLAY_QUIZZ,
+        MSG_DISPLAY_QUESTION,
         MSG_TITLE_QUIZZ_CHANGED,
         MSG_QUIZZ_CHANGED,
+        MSG_QUESTION_CHANGED,
         MSG_CLOSE_QUIZZ_TAB,
         // Pour pouvoir mettre à jour l'en-tête de l'onglet en cas de 
         // changement de titre de course
         MSG_COURSE_CHANGED,
+        MSG_ENONCE_QUESTION_CHANGED,
         MSG_CLOSE_TAB_COURSE,
-       // MSG_CLOSE_TAB_CATEGORY,
+        // MSG_CLOSE_TAB_CATEGORY,
         MSG_REFRESH_DATA,
         MSG_PROFILE,
         MSG_PROFILE_CHANGED,
         MSG_CLOSE_TAB_PROFILE,
+        MSG_CLOSE_QUESTION_TAB,
         MSG_UPDATE_PROFILE,
         MSG_ADD_REGISTRATION,
         //MSG_CATEGORY_CHANGED,
-        MSG_NAME_CHANGED,
-        MSG_CATEGORY_CHANGED
-
+        MSG_NAME_CHANGED
     }
 }
