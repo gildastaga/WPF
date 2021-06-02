d2358c11bf1c01303a2d2092340a6e48cfeab943
Marceline <marcelinealvinemomosohryahoo.fr>
2021-06-02 10:12:09 +0200

    impléméntation de l'interface

---
+++ prbd_School04/ViewModel/CamboboxViewModel.cs
@@ -0,0 +1,20 @@
+﻿using School04.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School04.ViewModel
+{
+    class CamboboxViewModel : ViewModelCommon
+    {
+        private ObservableCollection<Student> studentRegister;
+        public ObservableCollection<Student> StudentRegister
+        {
+            get => studentRegister;
+            set => SetProperty<ObservableCollection<Student>>(ref studentRegister, value);
+        }
+    }
+}
